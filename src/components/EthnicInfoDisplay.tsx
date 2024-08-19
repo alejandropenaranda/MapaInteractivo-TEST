05582c7f70c06e73468f2cbe2c76701459e45a4e
apenaranda <alejandropenaranda3001@gmail.com>
2024-08-19 11:24:15 -0500

    add responsive and copy message

--- src/components/EthnicInfoDisplay.tsx
+++ src/components/EthnicInfoDisplay.tsx
@@ -1,44 +1,4 @@
-// import React from 'react';
-// import '../styles/EthnicInfoDisplay.css';
-
-// interface EthnicInfoDisplayProps {
-//   content: string;
-//   link?: string;
-//   setViewerUrl: (url: string | undefined) => void;
-//   setReferenceStatus: (visible: boolean | undefined) => void;
-// }
-
-// const EthnicInfoDisplay: React.FC<EthnicInfoDisplayProps> = ({ content, link, setViewerUrl, setReferenceStatus }) => {
-
-
-//   const handleLinkClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-//     event.preventDefault();
-//     if (link) {
-
-//       setViewerUrl(link);
-//       setReferenceStatus(true);
-//       console.log("Estoy cambiando el estado a true")
-//       console.log(link)
-//     }
-//   };
-
-//   return (
-//     <div className="ethnic-info-display">
-//       {content && <p>{content}</p>}
-//       {link && (
-//         <div className="button-container-EID">
-//           <button onClick={handleLinkClick}>
-//             Ver referencia
-//           </button>
-//         </div>
-//       )}
-//     </div>
-//   );
-// };
-
-// export default EthnicInfoDisplay;
-
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/EthnicInfoDisplay.css';
 
 interface EthnicInfoDisplayProps {
@@ -49,6 +9,8 @@ interface EthnicInfoDisplayProps {
 }
 
 const EthnicInfoDisplay: React.FC<EthnicInfoDisplayProps> = ({ content, link, setViewerUrl, setReferenceStatus }) => {
+  const [copyMessageVisible, setCopyMessageVisible] = useState(false);
+
   const handleLinkClick = (event: React.MouseEvent<HTMLButtonElement>) => {
     event.preventDefault();
     if (link) {
@@ -61,7 +23,10 @@ const EthnicInfoDisplay: React.FC<EthnicInfoDisplayProps> = ({ content, link, se
 
   const handleCopyClick = () => {
     navigator.clipboard.writeText(content).then(() => {
-      alert('Se ha copiado el contenido');
+      setCopyMessageVisible(true);
+      setTimeout(() => {
+        setCopyMessageVisible(false);
+      }, 3000); // Oculta el mensaje después de 2 segundos
     }).catch(err => {
       console.error('Error al copiar al portapapeles: ', err);
     });
@@ -71,69 +36,20 @@ const EthnicInfoDisplay: React.FC<EthnicInfoDisplayProps> = ({ content, link, se
     <div className="ethnic-info-display">
       {content && <p>{content}</p>}
       <div className="button-container-EID">
+        <div className="copy-message-container">
+          {copyMessageVisible && <span className="copy-message">!Se ha copiado correctamente!</span>}
+          <button onClick={handleCopyClick}>
+            Copiar
+          </button>
+        </div>
         {link && (
           <button onClick={handleLinkClick}>
             Ver referencia
           </button>
         )}
-        <button onClick={handleCopyClick}>
-          Copiar
-        </button>
       </div>
     </div>
   );
 };
 
 export default EthnicInfoDisplay;
-
-// import React, { useState } from 'react';
-// import '../styles/EthnicInfoDisplay.css';
-
-// interface EthnicInfoDisplayProps {
-//   content: string;
-//   link?: string;
-//   setViewerUrl: (url: string | undefined) => void;
-//   setReferenceStatus: (visible: boolean | undefined) => void;
-// }
-
-// const EthnicInfoDisplay: React.FC<EthnicInfoDisplayProps> = ({ content, link, setViewerUrl, setReferenceStatus }) => {
-//   const [copySuccess, setCopySuccess] = useState<string | null>(null);
-
-//   const handleLinkClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-//     event.preventDefault();
-//     if (link) {
-//       setViewerUrl(link);
-//       setReferenceStatus(true);
-//       console.log("Estoy cambiando el estado a true");
-//       console.log(link);
-//     }
-//   };
-
-//   const handleCopyClick = () => {
-//     navigator.clipboard.writeText(content).then(() => {
-//       setCopySuccess('Contenido copiado al portapapeles');
-//       setTimeout(() => setCopySuccess(null), 2000);
-//     }).catch(err => {
-//       console.error('Error al copiar al portapapeles: ', err);
-//     });
-//   };
-
-//   return (
-//     <div className="ethnic-info-display">
-//       {content && <p>{content}</p>}
-//       <div className="button-container-EID">
-//         {link && (
-//           <button onClick={handleLinkClick}>
-//             Ver referencia
-//           </button>
-//         )}
-//         <button onClick={handleCopyClick}>
-//           Copiar contenido
-//         </button>
-//       </div>
-//       {copySuccess && <div className="copy-alert">{copySuccess}</div>}
-//     </div>
-//   );
-// };
-
-// export default EthnicInfoDisplay;
--- src/components/EthnicInfoViewer.tsx
+++ src/components/EthnicInfoViewer.tsx
@@ -1,50 +1,3 @@
-// import React, { useEffect, useState } from 'react';
-// import '../styles/EthnicInfoViewer.css';
-
-// interface EthnicInfoViewerProps {
-//   initialUrl?: string;
-//   status?: boolean;
-//   setReferenceStatus: (visible: boolean | undefined) => void;
-// }
-
-// const EthnicInfoViewer: React.FC<EthnicInfoViewerProps> = ({ initialUrl}) => { // setReferenceStatus}) => {
-//   const [url, setUrl] = useState<string | undefined>(initialUrl);
-
-//   useEffect(() => {
-//     setUrl(initialUrl);
-//   }, [initialUrl]);
-
-//   const handleVisitReference = () => {
-//     if (url) {
-//       window.open(url, '_blank');
-//     }
-//   };
-
-//   return (
-//     <div>
-//         <div className="ethnic-info-viewer">
-//         <div className="title-container">
-//           <h2>Repositorio</h2>
-//         </div>  
-//           <iframe
-//             src={url}
-//             title="Reference"
-//             className="reference-iframe"
-//           />
-//           {url && (
-//           <div className="button-container-EIV">
-//             <button onClick={handleVisitReference}>
-//               Visitar referencia
-//             </button>
-//           </div>
-//           )} 
-//         </div>
-//     </div>
-//   );
-// };
-
-// export default EthnicInfoViewer;
-
 import React, { useEffect, useState } from 'react';
 import '../styles/EthnicInfoViewer.css';
 
@@ -54,7 +7,7 @@ interface EthnicInfoViewerProps {
   setReferenceStatus: (visible: boolean | undefined) => void;
 }
 
-const EthnicInfoViewer: React.FC<EthnicInfoViewerProps> = ({ initialUrl }) => { // setReferenceStatus}) => {
+const EthnicInfoViewer: React.FC<EthnicInfoViewerProps> = ({ initialUrl }) => {
   const [url, setUrl] = useState<string | undefined>(initialUrl);
 
   useEffect(() => {
